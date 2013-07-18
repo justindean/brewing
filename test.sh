3c7a79d053e334ee31cc4a6fa9050204d515262c
jdean <jdean@shopzilla.com>
2013-07-18 08:53:56 -0700

    updating test.sh to execute xml recipe cleanup

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-for i in `find recipes -name *.xml -mmin -30`;do python recipe_file_cleanup.py $i; done
+for i in `find recipes -name *.xml -mmin -30`;do python recipe_file_cleanup.py $i && git add $i; done
 ls recipes/
+git push
 #test
 exit 0
