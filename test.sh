109c9e5752bccb999331fb441b1a8dc10d62baf5
jdean <jdean@shopzilla.com>
2013-07-18 09:00:51 -0700

    updating test.sh to execute xml recipe cleanup

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-for i in `find recipes -name *.xml -mmin -30`;do python recipe_file_cleanup.py $i && git add $i; done
+for i in `find recipes -name *.xml -mmin -30`;do python recipe_file_cleanup.py $i; done
 ls recipes/
+for i in `find recipes -name *.clean -mmin -10`; do git add $1; done
+git commit -a -m "Travis-CI Checking in updated recipe files"
 git push
 #test
 exit 0
